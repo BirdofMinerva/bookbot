611034f46593c989c4991b3a88e76f1b99b5019f
0wl <strieglerkirsten.robin@gmail.com>
2024-05-17 16:00:55 +0200

    creating main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+def main():
+    path = "/home/luna/Documents/Workspace/Github/BirdofMinerva/bookbot/books/frankenstein.txt"
+
+
+    with open(path, "r") as book:
+        read = book.read()
+        lowread = read.lower()
+        dic = {
+            
+            "a": 0,
+            "b": 0,
+            "c": 0,
+        }
+
+main()
+
